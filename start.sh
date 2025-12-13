6f75d604cb7744dc25d19907d8e1a6b6da04e302
tin1914 <tin.rodriguez.2224@gmail.com>
2025-12-13 17:21:38 +0800

    requirements

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 export TF_CPP_MIN_LOG_LEVEL=2
 export CUDA_VISIBLE_DEVICES=""
-gunicorn app:app --bind 0.0.0.0:${PORT:-8000} --workers=1 --timeout=120 --max-requests=100 --max-requests-jitter=10
+export TF_FORCE_GPU_ALLOW_GROWTH=true
+export OMP_NUM_THREADS=1
+gunicorn app:app --bind 0.0.0.0:${PORT:-8000} --workers=1 --timeout=300 --max-requests=10 --max-requests-jitter=5 --worker-class=sync --preload
